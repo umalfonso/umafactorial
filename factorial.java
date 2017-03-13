6602b8b5918ea34d12b3173fcd6d1b023f4ba7ca
Alfonso <umalfonso@uma.es>
2017-03-13 20:01:51 +0100

    implementacion inicial

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class factorial {
+	
+	public static void main(Sring []args) {
+		Scanner scanInput = new Scanner(System.in);
+		System.out.print("Introduzca un numero: ");
+		long num = scanInput.nextLong();
+		long fac = 1;
+		for (long i=1; i<=num; i++) {
+			fac = fac * i;
+		}	
+		System.out.println("El factorial de "+num+" es "+fac);
+		scanInput.close();
+	}
+}
